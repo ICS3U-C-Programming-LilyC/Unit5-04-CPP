60afa8598534022fccfc000b120549fcfd8f4c60
Lily Carroll <lily.carroll@stu.ocsb.ca>
2023-12-03 02:29:09 +0000

    Added rounding

--- calculator.cpp
+++ calculator.cpp
@@ -9,8 +9,9 @@
 // of your 2 numbers.
 
 #include <cmath>
+#include <iomanip>
 #include <iostream>
-#include<string>
+#include <string>
 
 // Function that checks what sign will
 // be used in the calculation.
@@ -29,58 +30,62 @@ float Calculate(char sign, float firstNumber, float secondNumber) {
     } else if (sign == '/') {
         answer = firstNumber / secondNumber;
     } else if (sign == '%') {
-        // Using built in modulo function (fmod)
+        // Using built-in modulo function (fmod)
         answer = fmod(firstNumber, secondNumber);
     }
+    // Returning the value of answer to my main() function.
     return answer;
 }
 
 int main() {
     // Declaring variables.
-    std::string firstNumber;
-    std::string secondNumber;
+    std::string firstNumberStr;
+    std::string secondNumberStr;
     float answer;
     char sign;
 
     // Getting user input for number1.
     std::cout << "Enter your first number as a decimal: ";
-    std::cin >> firstNumber;
+    std::cin >> firstNumberStr;
 
     // Getting user input for number2.
     std::cout << "Enter your second number as a decimal: ";
-    std::cin >> secondNumber;
+    std::cin >> secondNumberStr;
 
     // Getting user input for mathematical sign.
     std::cout << "Enter a mathematical sign (+, -, *, /, %): ";
     std::cin >> sign;
 
     // Using if statement to check
-    // if mathematical sign is valid.
+    // if the mathematical sign is valid.
     // If it is valid then their numbers
     // will be converted into decimals
-    // and error check using a try catch.
+    // and error check using a try-catch.
     if (sign == '+' || sign == '-' ||
-    sign == '*' || sign == '/' || sign == '%') {
-        // Using a try catch to catch any errors.
+        sign == '*' || sign == '/' || sign == '%') {
+        // Using a try-catch to catch any errors.
         // Converting number1 and number2 into decimals.
         try {
-            // cast the numbers to a float
-            float firstNumberAsFloat = std::stof(firstNumber);
-            float secondNumberAsFloat = std::stof(secondNumber);
+            // Convert the numbers from strings to floats.
+            float firstNumber = std::stof(firstNumberStr);
+            float secondNumber = std::stof(secondNumberStr);
 
-            // call the function
-            answer = Calculate(sign, firstNumberAsFloat, secondNumberAsFloat);
+            // Calling calculator() function.
+            answer = Calculate(sign, firstNumber, secondNumber);
 
-            // Displaying the answer to the user.
-            std::cout << firstNumberAsFloat << sign
-            << secondNumberAsFloat << " = " << answer;
+            // Displaying the answer to the user with 2 decimal places.
+            std::cout << std::fixed << std::setprecision(2)
+                      << firstNumber << " " << sign << " "
+                      << secondNumber << " = " << answer;
         // Catching any errors.
         } catch (std::invalid_argument) {
             std::cout << "Invalid number.";
         }
-    // Using else statement to display an error
-    // message when the mathematical sign is invalid.
+        // Using else statement to display an error
+        // message when the mathematical sign is invalid.
     } else {
         std::cout << sign << " is an invalid mathematical sign.";
     }
+
+    return 0;
 }
